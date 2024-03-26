0b8c041bb4ce9044b0261763fe7d1077d4108720
Jonatas da Silva Teixeira <jonatasteixeira@id.uff.br>
2024-03-26 16:43:24 -0300

    feat: including api server rsvp form

---
+++ server.js
@@ -0,0 +1,24 @@
+// server.js
+const fastify = require('fastify')();
+const saveConfirmation = require('./app/api/rsvp/saveConfirmation');
+const fastifyCors = require('@fastify/cors');
+
+// Registre o plugin CORS
+fastify.register(fastifyCors, {
+ origin: true, // Permite requisições de qualquer origem
+});
+
+// Registre o plugin saveConfirmation
+fastify.register(saveConfirmation);
+
+// Inicie o servidor
+const start = async () => {
+ try {
+    await fastify.listen({ port: 3000 });
+    console.log(`Server running at http://localhost:3000`);
+ } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+ }
+};
+start();
