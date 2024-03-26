7e28a49945dcb493a46c6561dc19ec605ddd3f54
Jonatas da Silva Teixeira <jonatasteixeira@id.uff.br>
2024-03-26 16:39:34 -0300

    chore: update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,9 @@
 import { MetadataRoute } from "next";
 import prisma from "@/lib/prisma";
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+export default async function sitemap(
+  prisma: any,
+): Promise<MetadataRoute.Sitemap> {
   const users = await prisma.user.findMany({
     select: {
       id: true,
@@ -14,7 +16,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       url: "https://precedent.dev",
       lastModified: new Date(),
     },
-    ...users.map((user) => ({
+    ...users.map((user: { id: any }) => ({
       url: `https://precedent.dev/${user.id}`,
       lastModified: new Date(),
     })),
